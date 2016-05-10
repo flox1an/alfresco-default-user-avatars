a79f5c4f7a58d758889c276ef0b674a732dd5a0f
Peter Löfgren <peter@loftux.se>
2016-05-10 16:21:53 +0200

    New version 1.1.1. Minor tweaks to run.sh

--- alfresco-share/run.sh
+++ alfresco-share/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Downloads the spring-loaded lib if not existing and runs the Share AMP
-springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.0.RELEASE/springloaded-1.2.0.RELEASE.jar
+springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.5.RELEASE/springloaded-1.2.5.RELEASE.jar
 
 if [ ! -f $springloadedfile ]; then
 mvn validate -Psetup
